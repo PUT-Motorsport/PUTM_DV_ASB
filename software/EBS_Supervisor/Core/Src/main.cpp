a2965328faa7b1e8d151135488ea768368d5e541
Piotr Lesicki <pile.atinea@tme.eu>
2023-03-09 23:08:20 +0100

    written most of the logic

--- software/EBS_Supervisor/Core/Src/main.cpp
+++ software/EBS_Supervisor/Core/Src/main.cpp
@@ -23,10 +23,11 @@
 /* USER CODE BEGIN Includes */
 
 #include <functional>
+#include <math.h>
 
-#include "werker.hpp"
 #include "timer.hpp"
 #include "gpioElements.hpp"
+#include "PUTM_EV_CAN_LIBRARY/lib/can_interface.hpp"
 
 /* USER CODE END Includes */
 
@@ -46,6 +47,12 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
+struct Fun
+{
+	float a;
+	float b;
+	float solve(float x) { return a * x + b; }
+};
 
 /* USER CODE END PM */
 
@@ -56,23 +63,20 @@ DMA_HandleTypeDef hdma_adc1;
 CAN_HandleTypeDef hcan;
 
 TIM_HandleTypeDef htim3;
+
 /* USER CODE BEGIN PV */
 
 const int adc_count = 3;
 
 volatile uint16_t adc_dma_buffer[3];
 
-Werker * werker;
+uint16_t can_timeout_counter;
 
-StopWerker stop_werker(&werker);
-ErrorWerker err_werker(&werker);
-ContinuousMonitoringWerker monitoring_werker(&werker, &err_werker, &stop_werker);
-InitialCheckUpWerker init_werker(&werker, &err_werker, &monitoring_werker);
+volatile Fun air_fun = { 0.0026308866087872f, -2.00210470928703f };
 
-#if USE_TEST
-TestWerker test_werker1(&werker);
-TestWerker test_werker2(&werker, &test_werker1);
-#endif
+volatile float air_ebs;
+volatile float air_redundant;
+volatile float air_main;
 
 /* GPIO Section */
 GpioOutElement led_ok(LD_OK_GPIO_Port, LD_OK_Pin);
@@ -91,14 +95,37 @@ GpioOutElement valve2(VALVE2_GPIO_Port, VALVE2_Pin);
 
 GpioInElement sdc_ready(SDC_RDY_GPIO_Port, SDC_RDY_Pin);
 
-/* Tests */
+static struct Config
+{
+	constexpr float brake_lower_bound = 6.5f;
+	constexpr float brake_upper_bound = 8.f;
+	constexpr float brake_press_deviation = 0.01f;
+	constexpr float can_brake_offset = -0.01f;
+	constexpr float press_tf_coef = 1.f;
+	constexpr float can_brake_lower_bound = 1.f;
+	constexpr float can_engaged_brakes_upper_bound = 90.f;
+	constexpr float can_engaged_brakes_lower_bound = 60.f;
 
+	constexpr uint32_t sdc_settle_timeout = 100;
+	constexpr uint32_t valve_settle_delay = 200;
+	constexpr uint32_t valve_settle_timeout = 200;
+
+	constexpr uint16_t can_timeout = 100;
+}volatile config;
+
+/* Tests */
 #if USE_TEST_POINTS
 static struct TestPoints
 {
-	volatile const uint16_t &air_ebs_adc = adc_dma_buffer[0];
-	volatile const uint16_t &air_redundant = adc_dma_buffer[1];
-	volatile const uint16_t &air_main = adc_dma_buffer[2];
+	struct ADC
+	{
+		volatile const uint16_t &air_ebs_adc = adc_dma_buffer[0];
+		volatile const uint16_t &air_redundant_adc = adc_dma_buffer[1];
+		volatile const uint16_t &air_main_adc = adc_dma_buffer[2];
+	}volatile adc;
+	volatile float &air_ebs_ = air_ebs;
+	volatile float &air_redundant_adc_ = air_redundant;
+	volatile float &air_main_adc_ = air_main;
 
 	volatile bool sdc_ready = false;
 } volatile test_points;
@@ -115,10 +142,9 @@ static void MX_CAN_Init(void);
 static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
 
-#if USE_TEST
-void test_fun1();
-void test_fun2();
-#endif
+void starTogglingWatchdog();
+void stopTogglingWatchdog();
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -158,33 +184,151 @@ int main(void)
   MX_CAN_Init();
   MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
-	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
 	HAL_ADCEx_Calibration_Start(&hadc1);
 	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_dma_buffer, adc_count);
 
-#if USE_TEST
-	werker = &test_werker2;
+	//--------------------------------------------------------------------------------------------------------------------
+	//Initial checkup
 
-	test_werker1.fun = &test_fun1;
-	test_werker2.fun = &test_fun2;
-#else
-	werker = &init_werker;
-#endif
+	//check if sdc is working
+	Timer sdc_timeout_timer(config.sdc_settle_timeout);
+	starTogglingWatchdog();
+	while(true)
+	{
+		if(sdc_timeout_timer.checkIfTimedOutThenReset()) Error_Handler();
+		if(sdc_ready.isActive()) break;
+	}
+
+	sdc_timeout_timer.restart();
+	stopTogglingWatchdog();
+	while(true)
+	{
+		if(sdc_timeout_timer.checkIfTimedOutThenReset()) Error_Handler();
+		if(!sdc_ready.isActive()) break;
+	}
+
+	starTogglingWatchdog();
+
+	//check pressure
+	if(std::abs(air_ebs - air_redundant) > config.brake_press_deviation) Error_Handler();
+	if(config.brake_lower_bound >= air_ebs || air_ebs >= config.brake_upper_bound) Error_Handler();
+	if(config.brake_lower_bound >= air_redundant || air_redundant >= config.brake_upper_bound) Error_Handler();
+
+	auto aq_main = PUTM_CAN::can.get_aq_main();
+
+	//aq_main.break_press returns in kP and we use Bars, so times 0.01
+	float brake_press_front = aq_main.brake_pressure_front * 0.01f + config.can_brake_offset;
+	float brake_press_rear = aq_main.brake_pressure_rear * 0.01f + config.can_brake_offset;
+
+	if(brake_press_front < config.press_tf_coef * air_ebs) Error_Handler();
+	if(brake_press_rear < config.press_tf_coef * air_ebs) Error_Handler();
+
+	//wait for tc enabled
+	bool tc_activated = false;
+	do
+	{
+		HAL_Delay(100);
+		auto tc_main = PUTM_CAN::can.get_tc_main();
+		tc_activated = tc_main.traction_control_enable;
+	} while(!tc_activated);
+
+	//check valves
+	valve1.activate();
+	valve2.deactivate();
+	HAL_Delay(config.valve_settle_delay);
+	Timer valve_timeout_timer(config.valve_settle_timeout);
+	while(true)
+	{
+		if(valve_timeout_timer.checkIfTimedOutThenReset()) Error_Handler();
+		if(PUTM_CAN::can.get_aq_main_new_data())
+		{
+			aq_main = PUTM_CAN::can.get_aq_main();
+
+			brake_press_front = aq_main.brake_pressure_front * 0.01f + config.can_brake_offset;
+			brake_press_rear = aq_main.brake_pressure_rear * 0.01f + config.can_brake_offset;
+		}
+		if(brake_press_front > config.press_tf_coef * air_ebs && brake_press_rear < config.can_brake_lower_bound) break;
+	}
+	valve1.activate();
+	valve2.deactivate();
+	HAL_Delay(config.valve_settle_delay);
+	valve_timeout_timer.restart();
+	while(true)
+	{
+		if(valve_timeout_timer.checkIfTimedOutThenReset()) Error_Handler();
+		if(PUTM_CAN::can.get_aq_main_new_data())
+		{
+			aq_main = PUTM_CAN::can.get_aq_main();
+
+			brake_press_front = aq_main.brake_pressure_front * 0.01f + config.can_brake_offset;
+			brake_press_rear = aq_main.brake_pressure_rear * 0.01f + config.can_brake_offset;
+		}
+		if(brake_press_rear > config.press_tf_coef * air_ebs && brake_press_front < config.can_brake_lower_bound) break;
+	}
+
+
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+	//--------------------------------------------------------------------------------------------------------------------
+	//continuous monitoring
+	auto apps_main = PUTM_CAN::can.get_apps_main();
+	auto prev_apps_counter_value = apps_main.counter;
 	while (1)
 	{
-#if USE_TEST
-		test_points.sdc_ready = sdc_ready.isActive();
-#endif
-		werker->run();
+		//check sdc
+		if(!sdc_ready.isActive())
+		{
+			HAL_Delay(config.valve_settle_delay);
+			valve_timeout_timer.restart();
+			while(true)
+			{
+				if(valve_timeout_timer.checkIfTimedOutThenReset()) Error_Handler();
+				if(PUTM_CAN::can.get_aq_main_new_data())
+				{
+					aq_main = PUTM_CAN::can.get_aq_main();
+
+					brake_press_front = aq_main.brake_pressure_front * 0.01f + config.can_brake_offset;
+					brake_press_rear = aq_main.brake_pressure_rear * 0.01f + config.can_brake_offset;
+				}
+				if(config.can_engaged_brakes_lower_bound < brake_press_rear  && config.can_engaged_brakes_upper_bound > brake_press_rear &&
+				   config.can_engaged_brakes_lower_bound < brake_press_front && config.can_engaged_brakes_upper_bound > brake_press_front) break;
+			}
+			break;
+		}
+
+		//check if can alive
+		auto apps_main = PUTM_CAN::can.get_apps_main();
+		counter_value = apps_main.counter();
+		if(prev_apps_counter_value = counter_value) can_timeout_counter++;
+		else can_timeout_counter = 0;
+		if(can_timeout_counter >= config.can_timeout) Error_Handler();
+
+		//check Ass
+		//TODO: do ustalenia skąd mam to niby brać
+
+		//check RES
+		//TODO: też do ustalenia
+
+		//check pressure
+		if(std::abs(air_ebs - air_redundant) > config.brake_press_deviation) Error_Handler();
+		if(config.brake_lower_bound >= air_ebs || air_ebs >= config.brake_upper_bound) Error_Handler();
+		if(config.brake_lower_bound >= air_redundant || air_redundant >= config.brake_upper_bound) Error_Handler();
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
 	}
+
+	//--------------------------------------------------------------------------------------------------------------------
+	//Stop monitoring
+	stopTogglingWatchdog();
+
+	//TODO: idk if in stop monitoring mode sth should be done
+
+	while(true) {}
   /* USER CODE END 3 */
 }
 
@@ -201,12 +345,11 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -222,7 +365,7 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
   {
     Error_Handler();
   }
@@ -461,39 +604,21 @@ static void MX_GPIO_Init(void)
 
 void HAL_ADC_ConsCpltCallback(ADC_HandleTypeDef *hadc)
 {
+	air_ebs = air_fun.solve(float(adc_dma_buffer[0]));
+	air_redundant_adc = air_fun.solve(float(adc_dma_buffer[1]));
+	air_main = air_fun.solve(float(adc_dma_buffer[2]));
 	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_dma_buffer, adc_count);
 }
 
-#if USE_TEST
-
-void test_fun1()
+void starTogglingWatchdog()
 {
-	Timer led_timer(500);
-	while(true)
-	{
-		if(led_timer.checkIfTimedOutThenReset())
-		{
-			led_ok.toggle();
-		}
-	}
+	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
 }
 
-void test_fun2()
+void stopTogglingWatchdog()
 {
-	Timer led_timer(1000);
-	Timer timer(2000);
-	while(true)
-	{
-		if (led_timer.checkIfTimedOutThenReset()) led_ok.activate();
-		if (timer.checkIfTimedOutThenReset())
-		{
-			werker = &test_werker1;
-			return;
-		}
-	}
+	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_2);
 }
-
-#endif
 
 /* USER CODE END 4 */
 
@@ -508,9 +633,22 @@ void Error_Handler(void)
   __disable_irq();
 
   led_err.activate();
+  stopTogglingWatchdog();
 
+  Timer can_send_timer(100);
   while (1)
   {
+	  if(can_send_timer.checkIfTimedOutThenReset())
+	  {
+		  PUTM_CAN::DV_Ass dv_ass
+		  {
+			  .ass = PUTM_CAN::AutonomousSystemStatus::Emergency,
+		  };
+		  PUTM_CAN::Can_tx_message<PUTM_CAN::DV_Ass> can_sender(dv_ass, PUTM_CAN::can_tx_header_DV_ASS);
+		  if(can_sender.send(hcan1) != HAL_StatusTypeDef::HAL_OK) led_chk.activate();
+	  }
+
+	//TODO: wysyłanie ramki do ASSI
   }
   /* USER CODE END Error_Handler_Debug */
 }
